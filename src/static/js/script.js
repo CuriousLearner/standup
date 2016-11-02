72423c682651b2c6a4d80512191823c2a78dcafc
Nikhil Handa <nikhilhanda@live.com>
2016-11-02 20:27:19 +0530

    Navbar checks for active page and highlights it

--- src/static/js/script.js
+++ src/static/js/script.js
@@ -1,6 +1,12 @@
 (function() {
   $('.navbar-toggle').click(handleIcon);
+  var path = window.location.pathname.split('/')[1];
+  handleActive(path);
 
+  function handleActive(path){
+    $('.'+path).removeClass('nav-bttn');
+    $('.'+path).addClass('actv');
+  }
   function handleIcon(){
     $('.icon-bar').toggle();
     $('.icon-remove').toggle();
